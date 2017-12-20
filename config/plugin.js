331f4b7a06da4979144098fb88a388d4ff8d29ad
Eden Liu <eden90267@gmail.com>
2017-12-20 16:13:03 +0800

    Step2

---
+++ config/plugin.js
@@ -0,0 +1,5 @@
+const path = require('path');
+exports.ua = {
+  enable: true,
+  path: path.join(__dirname, '../lib/plugin/egg-ua')
+};
---
+++ test/index.test.js
@@ -0,0 +1,20 @@
+const {app} = require('egg-mock/bootstrap');
+
+describe('test/index.test.js', () => {
+
+  it('should GET / with iOS', () => {
+    return app.httpRequest()
+        .get('/')
+        .set('user-agent', 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1')
+        .expect(200)
+        .expect('isIOS: true');
+  });
+
+  it('should GET / with iOS', () => {
+    return app.httpRequest()
+        .get('/')
+        .expect(200)
+        .expect('isIOS: false');
+  })
+
+});
